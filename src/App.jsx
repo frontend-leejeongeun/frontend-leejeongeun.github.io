084385bc492124b996fdc205d4d0b09b0fe0e8d9
frontenddeveloper-leejeongeun <49803637+frontend-leejeongeun@users.noreply.github.com>
2025-02-13 23:50:23 +0900

    라우팅 이슈로 hashrouter로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import TodoVanilla from "./pages/TodoVanilla";
 import TodoReact from "./pages/TodoReact";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/todovanilla" element={<TodoVanilla />} />
